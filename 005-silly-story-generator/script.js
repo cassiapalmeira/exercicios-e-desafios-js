a8533b8d6d1b67dfe7d2d4bd8a059392f17067e1
Cássia Palmeira <cassia.palmeira.dev@gmail.com>
2023-04-17 17:21:31 -0300

    Update script.js
    Retirei os ";" do js

--- 005-silly-story-generator/script.js
+++ 005-silly-story-generator/script.js
@@ -1,10 +1,10 @@
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
+const customName = document.getElementById('customname')
+const randomize = document.querySelector('.randomize')
+const story = document.querySelector('.story')
 
 function randomValueFromArray(array){
-  const random = Math.floor(Math.random() * array.length);
-  return array[random];
+  const random = Math.floor(Math.random() * array.length)
+  return array[random]
 }
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
@@ -15,13 +15,13 @@ let insertY = ['the soup kitchen', 'Disneyland', 'the White House'] // Array 2
 
 let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'] // Array 3
 
-randomize.addEventListener('click', result); // event listener
+randomize.addEventListener('click', result) // event listener
 
 function result() {
-  let newStory = storyText;
-  let xItem = randomValueFromArray(insertX); // sorteia algum item da Array 1
-  let yItem = randomValueFromArray(insertY); // sorteia algum item da Array 2
-  let zItem = randomValueFromArray(insertZ); // sorteia algum item da Array 3
+  let newStory = storyText
+  let xItem = randomValueFromArray(insertX) // sorteia algum item da Array 1
+  let yItem = randomValueFromArray(insertY) // sorteia algum item da Array 2
+  let zItem = randomValueFromArray(insertZ) // sorteia algum item da Array 3
   
   // substituem os ':insert:' por algum item de alguma das arrays
   newStory = newStory.replace(':insertx:', xItem)
@@ -30,18 +30,18 @@ function result() {
   newStory = newStory.replace(':insertx:', xItem)
 
   if(customName.value !== '') { // altera o nome para o que for inserido na caixa de texto
-    const name = customName.value;
+    const name = customName.value
     newStory = newStory.replace('Bob', name)
   }
 
   if(document.getElementById("uk").checked) { // altera as medidas para as do Reino Unido
-    const weight = Math.round(300 / 14) + ' stone';
-    const temperature =  Math.round((94 - 32) / 1.8) + ' centigrade';
+    const weight = Math.round(300 / 14) + ' stone'
+    const temperature =  Math.round((94 - 32) / 1.8) + ' centigrade'
 
     newStory = newStory.replace('94 fahrenheit', temperature)
     newStory = newStory.replace('300 pounds', weight)
   }
 
-  story.textContent = newStory;
-  story.style.visibility = 'visible';
+  story.textContent = newStory
+  story.style.visibility = 'visible'
 }
