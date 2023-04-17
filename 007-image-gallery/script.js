8122e3e00899032ebc89367974f1bb88601b595f
Cássia Palmeira <cassia.palmeira.dev@gmail.com>
2023-04-17 17:24:01 -0300

    Update script.js
    Retirei os ";" do js

--- 007-image-gallery/script.js
+++ 007-image-gallery/script.js
@@ -1,11 +1,11 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector('.displayed-img')
+const thumbBar = document.querySelector('.thumb-bar')
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('button')
+const overlay = document.querySelector('.overlay')
 
 /* Declaring the array of image filenames */
-const imageFileNames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const imageFileNames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
 
 /* Declaring the alternative text for each image file */
@@ -20,31 +20,31 @@ const imageFileAlt = {
 
 /* Looping through images */
 for (var i = 0; i <= imageFileNames.length; i++) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/' + imageFileNames[i]);
-    newImage.setAttribute('alt', imageFileAlt[imageFileNames[i]]);
-    thumbBar.appendChild(newImage);
+    const newImage = document.createElement('img')
+    newImage.setAttribute('src', 'images/' + imageFileNames[i])
+    newImage.setAttribute('alt', imageFileAlt[imageFileNames[i]])
+    thumbBar.appendChild(newImage)
 
     newImage.addEventListener('click', function(e) {
-        displayedImage.src = e.target.src;
-        displayedImage.alt = e.target.alt;
-    });
+        displayedImage.src = e.target.src
+        displayedImage.alt = e.target.alt
+    })
 }
 
 
 /* Wiring up the Darken/Lighten button */
-btn.onclick = btnChange;
+btn.onclick = btnChange
 
 function btnChange() {
-    var classBtn = btn.getAttribute('class');
+    var classBtn = btn.getAttribute('class')
     
     if (classBtn === 'dark') {
-        btn.setAttribute('class', 'light');
-        btn.textContent = 'Lighten';
+        btn.setAttribute('class', 'light')
+        btn.textContent = 'Lighten'
         overlay.style.backgroundColor = 'rgba(0,0,0,0.5)'
     } else {
-        btn.setAttribute('class', 'dark');
-        btn.textContent = 'Darken';
+        btn.setAttribute('class', 'dark')
+        btn.textContent = 'Darken'
         overlay.style.backgroundColor = 'rgba(0,0,0,0)'
     }
 }
