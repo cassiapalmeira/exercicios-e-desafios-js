dea5dceb6225fc05cf542070f48a6bef74176a68
Cássia Palmeira <cassia.palmeira.dev@gmail.com>
2023-05-20 15:37:50 -0300

    Update script.js
    Adicionei animação e som aos botões.

--- 010-simon-game/script.js
+++ 010-simon-game/script.js
@@ -8,4 +8,9 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber] // seleciona uma das cores da array buttonColours e ela é armazenada na variável
 
     gamePattern.push(randomChosenColour) // a cor armazenada em randomChosenColour é adicionada ao final da array gamePattern
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100) // usando jQuery pra selecionar botão com a mesma id de randomChosenColour e, posteriormente, adicionando animação de flash no botão
+
+    var playSound = new Audio("sounds/" + randomChosenColour + ".mp3")
+    playSound.play() // a linha acima adiciona um som correspondente a cor selecionada e nessa linha o som é tocado
 }
